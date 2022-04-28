62ead5d0117da1e57bf670989d8c4532dabb29b9
wjrdjs005 <wjrdjs005@naver.com>
2022-04-28 12:03:17 +0900

    capter5clear

---
+++ react-complete-guide/src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Chart from '../Chart/Chart';
+
+const ExpensesChart = props =>{
+    const chartDataPoints = [
+        {label:'1월', value:0},
+        {label:'2월', value:0},
+        {label:'3월', value:0},
+        {label:'4월', value:0},
+        {label:'5월', value:0},
+        {label:'6월', value:0},
+        {label:'7월', value:0},
+        {label:'8월', value:0},
+        {label:'9월', value:0},
+        {label:'10월', value:0},
+        {label:'11월', value:0},
+        {label:'12월', value:0}
+    ];
+    console.log("ExpenseChart= props:" +props.date)
+    for(const expense of props.expensess){
+        const expenseMonth = expense.date.getMonth() // starting 0
+        chartDataPoints[expenseMonth].value += expense.amount;
+    }
+
+    return <Chart dataPoints={chartDataPoints}/>
+}
+
+export default ExpensesChart;
