3ad9e3bb6f77839b3ed9e1ee3ea711845ad8f574
wjrdjs005 <wjrdjs005@naver.com>
2022-04-29 23:16:30 +0900

    'commits'

---
+++ bookstore/src/components/UI/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Button = (props) =>{
+    return <button>props.child</button>
+}
+
+export default Button; 
---
+++ bookstore/src/components/book.js
@@ -0,0 +1,50 @@
+const book_list = [{
+    bookname: "불편한 편의점(40만부 기념 벚꽃 에디션)",
+    writer: "김호연",
+    publisher: "나무옆의자",
+    publish_date: new Date(2021,3,20),
+    book_price : 12600,
+    ebook_price : 9800,
+    book_img : '',
+    keyword : ['소설','위로','삶','독고','여사']
+},{
+    bookname: "운명을 바꾸는 부동산 투자 수업: 기초편",
+    writer: "정태익",
+    publisher: "리더스북",
+    publish_date: new Date(2022,2,22),
+    book_price : 15120,
+    ebook_price : 10800,
+    book_img : '',
+    keyword : ['투자자','인플레이션','월세','마인드','아파트']
+},
+{
+    bookname: "면접바이블 2.0",
+    writer: "이형",
+    publisher: "얼라이브북스",
+    publish_date: new Date(2022,3,12),
+    book_price : 17820,
+    ebook_price : 16830,
+    book_img : '',
+    keyword : ['유튜브','커리어','인성','취업','임원']
+},
+{
+    bookname: "2022 제13회 젊은작가상 수상작품집",
+    writer: "정태익",
+    publisher: "리더스북",
+    publish_date: new Date(2022,3,08),
+    book_price : 6930,
+    ebook_price : 5400,
+    book_img : '',
+    keyword : ['한국소설','소설집','장편','중 단편 소설','초파리']
+},
+{
+    bookname: "마음의 법칙",
+    writer: "폴커 키츠. 마누엘 투쉬",
+    publisher: "김희상",
+    publish_date: new Date(2022,1,10),
+    book_price : 14400,
+    ebook_price : 11200,
+    book_img : '',
+    keyword : ['투자자','인플레이션','월세','마인드','아파트']
+}
+]
