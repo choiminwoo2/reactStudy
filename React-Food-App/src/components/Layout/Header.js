b9d397578fab1725450cfa4c61ac692ad71a525c
wjrdjs005 <wjrdjs005@naver.com>
2022-05-07 22:32:43 +0900

    CartComponent end

--- React-Food-App/src/components/Layout/Header.js
+++ React-Food-App/src/components/Layout/Header.js
@@ -5,11 +5,14 @@ import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 import HeaderCartButton from "./HeaderCartButton";
+
+
+
 const Header = (props) => {
   return <React.Fragment>
       <header className={classes.header}>
           <h1>ReactMeals</h1>
-          <HeaderCartButton />
+          <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
           <img src={mealsImage} alt="A table of delicious food!" />
