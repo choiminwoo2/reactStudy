fbacb8df3c3306667d982bbd124ccd32537932ed
wjrdjs005 <wjrdjs005@naver.com>
2022-07-01 17:08:05 +0900

    natural redux commit

---
+++ Redux/redux-demo.js
@@ -0,0 +1,34 @@
+const redux = require('redux');
+
+
+const counterReducer = (state = {counter : 0 }, action) =>{
+
+    switch(action.type){
+        case 'increment' :
+            return { counter : state.counter +1}
+            break;
+        case 'decrement' :
+            return { counter : state.counter -1}
+            break;
+            
+    }
+
+
+    return state;
+}
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () =>{
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type:'increment'});
+store.dispatch({type:'decrement'});
+
+
+
+
