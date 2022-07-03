83c3555787f084f3e28910d50f180994bf0a685c
wjrdjs005 <wjrdjs005@naver.com>
2022-07-03 23:58:41 +0900

    redux prec

---
+++ Redux-async/src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import UiSlice from './ui-slice'
+import CartSlice from "./cart-slice";
+
+const store = configureStore({
+    reducer : {ui:UiSlice.reducer, cart: CartSlice.reducer }
+});
+
+export default store;
