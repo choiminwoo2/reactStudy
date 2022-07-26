9473f4710d6c730210fe7cf4189f736097a2ef88
wjrdjs005 <wjrdjs005@naver.com>
2022-07-26 23:32:08 +0900

    Next

--- NextInReact/first-app/components/layout/layout.tsx
+++ NextInReact/first-app/components/layout/layout.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import { Fragment } from "react";
-import styles from './layout.module.css'
+import styles from "./layout.module.css";
+type Props = {
+  children: React.ReactNode
+  home?: boolean;
+}
+
 const Layout = ({
   children,
   home,
-}: {
-  children: React.ReactNode;
-  home?: boolean;
-}) => {
+}: Props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +19,10 @@ const Layout = ({
         <meta name="description" content="Learn Next.js" />
       </Head>
       <header className={styles.header}>
-        {home ? (<Fragment></Fragment>) : (<Fragment></Fragment>)}
+        {home ? <Fragment>
+            <Image priority
+            src="/images"/>
+        </Fragment> : <Fragment></Fragment>}
       </header>
     </div>
   );
---
+++ NextInReact/first-app/pages/auth/index.tsx
@@ -0,0 +1,15 @@
+import { ReactComponentElement, ReactNode } from "react"
+interface AuthProps{
+    children : ReactNode;
+    id : string;
+    password: number;
+
+}
+
+
+const Auth = (props : AuthProps)  => {
+}
+
+
+
+export default Auth;
